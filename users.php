9715ea36999f9ac343f42ea15cd6eb871d3fb008
SidqiAlfareza <sidqi23@mhs.usk.ac.id>
2025-05-08 09:55:30 +0700

    Add files via upload

--- users.php
+++ users.php
@@ -5,6 +5,10 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Koneksi ke database
 require_once "../api/config.php";
 
